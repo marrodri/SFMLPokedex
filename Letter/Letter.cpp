87572fb26094e91efbfba58b18281ef352288570
marrodri <laza.mar64@gmail.com>
2023-11-07 11:54:21 -0800

    letter class added

---
+++ Letter/Letter.cpp
@@ -0,0 +1,48 @@
+//
+// Created by Marthel Rodriguez on 11/6/23.
+//
+
+#include "Letter.h"
+
+
+Letter::Letter() {
+}
+
+Letter::Letter(char characterCode, sf::Font &font, int fontSize) {
+    characterCode = characterCode;
+    text.setFont(font);
+    text.setString(characterCode);
+    text.setCharacterSize(fontSize);
+    glyphData = font.getGlyph(characterCode, fontSize, "regular");
+    text.setPosition(0,0);
+}
+
+void Letter::setPosition(float x, float y){
+    text.setPosition(x,y);
+}
+
+/**
+ * getters
+ * */
+
+sf::Vector2f Letter::getPosition() {
+    return text.getPosition();
+}
+
+sf::Glyph Letter::getGlyph() {
+    return glyphData;
+}
+
+/**
+ * SFML main methods
+ * */
+
+void Letter::eventHandler(sf::RenderWindow &window, sf::Event event) {
+}
+
+void Letter::update() {
+}
+
+void Letter::draw(sf::RenderTarget &target, sf::RenderStates states) const {
+    target.draw(text);
+}
---
+++ Letter/Letter.h
@@ -0,0 +1,37 @@
+//
+// Created by Marthel Rodriguez on 11/6/23.
+//
+
+#ifndef SFMLTEMPLATE_LETTER_H
+#define SFMLTEMPLATE_LETTER_H
+
+#include <SFML/Graphics.hpp>
+#include "../Components/Component.h"
+class Letter: public Component {
+private:
+    sf::Glyph glyphData;
+    sf::Text text;
+    char caracterCode;
+public:
+    Letter();
+    Letter(char characterCode, sf::Font &font, int fontSize);
+
+    /**sfml*/
+    void eventHandler(sf::RenderWindow &window, sf::Event event);
+
+    void update();
+
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+
+    void setPosition(float x, float y);
+
+    /**
+     * getters
+     * */
+    sf::Glyph getGlyph();
+    sf::Vector2f getPosition();
+
+};
+
+
+#endif //SFMLTEMPLATE_LETTER_H
