259d2396a0849b2c6348a20d7e8ea5fc60010bf9
marrodri <laza.mar64@gmail.com>
2023-11-24 23:52:20 -0800

    ButtonInterface added to GridButton and Logo

--- Screen/Logo/Logo.h
+++ Screen/Logo/Logo.h
@@ -6,9 +6,19 @@
 #define SFMLTEMPLATE_LOGO_H
 
 #include "../../Components/GUIComponent.h"
-class Logo :public GUIComponent{
+#include "../../Components/ButtonInterface.h"
+
+class Logo :public GUIComponent, private ButtonInterface{
 private:
     sf::RectangleShape logoContainer;
+
+    /**
+    * button interface methods
+    **/
+    void onClick() override;
+    void setOnClickFunction() override;
+    void onHoverSound() override;
+    void onClickSound() override;
 
 public:
     Logo();
